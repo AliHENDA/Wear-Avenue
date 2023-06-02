e18f73224b4d3b334c7e9d236128d5eb3ec54e61
Ali Henda <alihenda@aramisauto.local>
2023-06-02 19:18:59 +0200

    Management layout product rating

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,27 +3,62 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Category;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('picture')
-            ->add('gender')
+            ->add('name', TextType::class, [
+                'label' => 'Name'
+            ])
+            ->add('description', TextareaType::class)
+            ->add('picture', UrlType::class, [
+                'label' => 'Picture',
+
+            ])
+            ->add('gender', ChoiceType::class, [
+                'choices'  => [
+                    'Woman' => 'Woman',
+                    'Man' => 'Man',
+                    'Unisex' => 'Unisex'
+                ],
+                // Boutons radios
+                'expanded' => true, 
+                'label_attr' => [
+                    'class' => 'radio-inline',
+                ],         
+                'label' => 'Gender'     
+            ])
             ->add('color')
             ->add('rate')
-            ->add('price')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('slug')
-            ->add('best_sellers_order')
-            ->add('category')
+            ->add('price', MoneyType::class, [
+                'label' => 'Price'
+            ])
+            ->add('best_sellers_order', IntegerType::class)
+            ->add('category', EntityType::class, [
+                'label' => 'Category',
+                'class' => Category::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                //'multiple' => true,
+                //'expanded' => true,
+            ])
         ;
     }
 
