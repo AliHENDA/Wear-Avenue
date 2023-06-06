a95eba946988c6548a3ef7d8aac70043ec9be7e9
AliHENDA <alihenda4@gmail.com>
2023-06-07 00:03:06 +0200

    Bootstrap display BackOffice

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -6,18 +6,17 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('subtitle')
-            ->add('picture')
-            ->add('slug')
-            ->add('createdAt')
-            ->add('updatedAt')
+            ->add('name', TextType::class)
+            ->add('subtitle', TextareaType::class)
+            ->add('picture', TextType::class)
         ;
     }
 
