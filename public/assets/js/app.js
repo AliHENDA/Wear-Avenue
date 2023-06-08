4fdbfad03988dbaebe89fa7d004a2991977c6b53
AliHENDA <alihenda4@gmail.com>
2023-06-08 02:32:24 +0200

    not for this branch, but menu burger ok

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,41 +1,39 @@
-            var MenuItems = document.querySelector("#MenuItems");
 
-            MenuItems.style.maxHeight = "0px";
-                    
-            function menutoggle(){
+let menuBurger = document.querySelector(".menu-icon")
+MenuItems.style.height = "0px";
 
-                if(MenuItems.style.maxHeight == "Opx") 
-                {
-                    MenuItems.style.maxHeight = "200px";
-                }
-                else
-                {
-                    MenuItems.style.maxHeight = "Opx";
-                }
-            }
+menuBurger.addEventListener('click', ()=> {
+    if (MenuItems.style.height == "180px") {
+        MenuItems.style.height = "0";
+    } else {
+        MenuItems.style.height = "180px";
+    }
+})
 
-var ProductImg = document.getElementById("productImg");
-            var SmallImg = document.getElementsByClassName("small-img");
+let ProductImg = document.getElementById("productImg");
+let SmallImg = document.getElementsByClassName("small-img");
 
-            SmallImg[0].onclick = function() {
-                ProductImg.src = SmallImg[0].src;
-            }
-            SmallImg[1].onclick = function() {
-                ProductImg.src = SmallImg[1].src;
-            }
-            SmallImg[2].onclick = function() {
-                ProductImg.src = SmallImg[2].src;
-            }
+SmallImg[0].onclick = function () {
+    ProductImg.src = SmallImg[0].src;
+};
+SmallImg[1].onclick = function () {
+    ProductImg.src = SmallImg[1].src;
+};
+SmallImg[2].onclick = function () {
+    ProductImg.src = SmallImg[2].src;
+};
 
+function register() {
+    document.getElementById("RegisterForm").style.transform = "translateX(0px)";
+    document.getElementById("LoginForm").style.transform = "translateX(0px)";
+    indicator = document.getElementById("indicator").style.transform =
+        "translateX(100px)";
+}
 
-            function register(){
-                document.getElementById("RegisterForm").style.transform = "translateX(0px)";
-                document.getElementById("LoginForm").style.transform = "translateX(0px)";
-                indicator = document.getElementById("indicator").style.transform = "translateX(100px)";
-            }
-
-            function login(){
-                document.getElementById("RegisterForm").style.transform = "translateX(300px)";
-                document.getElementById("LoginForm").style.transform = "translateX(300px)";
-                indicator = document.getElementById("indicator").style.transform = "translateX(0px)";
-            }
+function login() {
+    document.getElementById("RegisterForm").style.transform =
+        "translateX(300px)";
+    document.getElementById("LoginForm").style.transform = "translateX(300px)";
+    indicator = document.getElementById("indicator").style.transform =
+        "translateX(0px)";
+}
